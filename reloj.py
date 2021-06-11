3ed6bdc22ea6003e8da9bcfeb0310c201c2da798
JuanICasareski <juani.casareski@gmail.com>
2021-06-11 14:51:40 -0300

    Corregi error de 'pasos'

--- reloj.py
+++ reloj.py
@@ -13,21 +13,22 @@ tft = st7789.ST7789(
 
 tft.init()  # Inicializacion del display
 
+WHITE = st7789.WHITE
 BLACK =  st7789.BLACK
 circulox = 120
 circuloy = 120
 radio = 90
 
-for hora in range(360):
-    for a in range(360):
-        x = int(cos(radians(a))*radio)
-        y = int(sin(radians(a))*radio)
-        tft.pixel(circulox+x, circuloy+y, BLACK)
+for hora in range(60):
+    for a in range(60):
+        x = int(cos(radians(a*6))*radio)
+        y = int(sin(radians(a*6))*radio)
+        tft.pixel(circulox+x, circuloy+y, WHITE)
 
         if a == hora:
-            tft.line(circulox, circuloy, circulox+x, circuloy+y, BLACK)
+            tft.line(circulox, circuloy, circulox+x, circuloy+y, WHITE)
         sleep(1)
-    #el clear acá
+    tft.fill(BLACK)
 
 
 
